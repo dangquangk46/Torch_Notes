6c57c2a572a0412e4378c0185ba8608783db5b86
NDQ <dangquang_k46@yahoo.com>
2020-03-04 11:50:27 +0700

    Update image_to_tensor.py

--- image_to_tensor.py
+++ image_to_tensor.py
@@ -1,3 +1,14 @@
+#transformation function 
+from torchvision import transforms
+preprocess = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        )])
+
 #std is standard deviation
 #mean is mean value
 img = Image.open('PATH')
